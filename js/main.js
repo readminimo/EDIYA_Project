2479458e97be338d379d89f58b29e4cdd3795a57
gyumk <gyukgm@gmail.com>
2020-04-08 12:21:16 +0900

    finish layout

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ window.onload = function(){
     var menuOpen = document.querySelector('.menu__open');
     var menuClose = document.querySelector('.menu__close');
     var menuNav = document.getElementById('menuNav');
-    var drink = document.querySelectorAll('.drink__dialog');
+    var drink__dialog = document.querySelectorAll('.drink__dialog');
+    var drink__item = document.querySelectorAll('.drink__item');
+    var drink__close = document.querySelectorAll('.drink__close');
 
     // 메뉴 슬라이드
     menuOpen.addEventListener('click', function (e) {
@@ -11,10 +13,19 @@ window.onload = function(){
     menuClose.addEventListener('click', function (e) {
         menuNav.classList.remove('isAct');
     })
-
-    // 확인용
-    // for (var i = 0; i < drink.length; i++) {
-    //     drink[i].hidden = false;
-    // }
-
+    
+    for (var a of drink__item){
+        a.addEventListener('click', function(e){
+            e.preventDefault();
+            this.classList.remove('close')
+            this.classList.add('open')
+        })
+    }
+    for(var b of drink__close){
+        b.addEventListener('click', function(e){
+            this.parentNode.parentNode.classList.remove('open')
+            this.parentNode.parentNode.classList.add('close')
+            e.stopPropagation();
+        })
+    }
 }
